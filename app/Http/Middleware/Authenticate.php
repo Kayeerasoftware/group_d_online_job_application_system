fce665f19069ff629c7ac5baf7c78cfbc959ea39
Kayeerasoftware <kayeerasoftware@gmail.com>
2026-05-12 06:10:49 +0300

    Remove brittle DB bootstrap workaround

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,23 +2,15 @@
 
 namespace App\Http\Middleware;
 
-use App\Support\DatabaseBootstrap;
 use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
-use Throwable;
 
 class Authenticate extends Middleware
 {
     public function handle($request, Closure $next, ...$guards)
     {
-        app(DatabaseBootstrap::class)->ensure();
-
-        try {
-            return parent::handle($request, $next, ...$guards);
-        } catch (Throwable) {
-            return redirect()->route('login');
-        }
+        return parent::handle($request, $next, ...$guards);
     }
 
     protected function redirectTo(Request $request): ?string

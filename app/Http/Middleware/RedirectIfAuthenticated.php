654e0ca5e2141c3eedaf83489b8e2fb1cddb3b86
Kayeerasoftware <kayeerasoftware@gmail.com>
2026-05-12 05:41:47 +0300

    Fix Laravel 12 auth middleware signatures

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -5,13 +5,11 @@ namespace App\Http\Middleware;
 use App\Support\DatabaseBootstrap;
 use Closure;
 use Illuminate\Auth\Middleware\RedirectIfAuthenticated as Middleware;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
 class RedirectIfAuthenticated extends Middleware
 {
-    public function handle(Request $request, Closure $next, string ...$guards): Response
+    public function handle($request, Closure $next, ...$guards)
     {
         app(DatabaseBootstrap::class)->ensure();
 

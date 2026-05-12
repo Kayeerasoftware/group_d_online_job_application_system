f42fea867beeb36926cf2f9214b5b189422442e6
Kayeerasoftware <kayeerasoftware@gmail.com>
2026-05-12 05:31:00 +0300

    Fix auth middleware signatures

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -4,11 +4,13 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Auth\Middleware\RedirectIfAuthenticated as Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
 class RedirectIfAuthenticated extends Middleware
 {
-    public function handle($request, Closure $next, ...$guards)
+    public function handle(Request $request, Closure $next, string ...$guards): Response
     {
         try {
             return parent::handle($request, $next, ...$guards);
